8ca6ca7aaa93799d7b25a55d07729eb6b9b15898
EOF-D <END-OFD@pm.me>
2025-04-06 16:18:08 -0400

    feat: started working on editting profile

--- src/client/pages/profile.tsx
+++ src/client/pages/profile.tsx
@@ -1,8 +1,17 @@
-import { User } from "@heroui/react";
-import { Card, CardHeader, CardBody, CardFooter, Divider } from "@heroui/react";
-
-import Layout from "@/client/layouts/default";
 import { useAuth } from "@/client/context/auth";
+import Layout from "@/client/layouts/default";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Input,
+  User,
+} from "@heroui/react";
+import { IconPencil } from "@tabler/icons-react";
+import { useState } from "react";
 
 /**
  * The profile page for the application.
@@ -11,6 +20,16 @@ function ProfilePage() {
   // Get the auth context to check if the user is logged in.
   const { user, isLoggedIn } = useAuth();
 
+  // State to manage editing the profile.
+  const [isEditing, setIsEditing] = useState(false);
+
+  // Handles save button.
+  const handleSave = () => {
+    // TODO: Implement save functionality.
+
+    setIsEditing(false);
+  };
+
   return (
     <Layout page="Home">
       <div className="flex flex-col items-center">
@@ -18,9 +37,63 @@ function ProfilePage() {
           <Card className="w-full max-w-sm p-4 ">
             <CardHeader>
               <User description={user?.email} name={user?.name} />
+              <div className="justify-end flex w-full">
+                {!isEditing ? (
+                  <Button
+                    size="sm"
+                    isIconOnly
+                    startContent={<IconPencil />}
+                    onPress={() => setIsEditing(!isEditing)}
+                  ></Button>
+                ) : (
+                  <div className="flex gap-2">
+                    <Button
+                      size="sm"
+                      variant="solid"
+                      color="primary"
+                      onPress={handleSave}
+                    >
+                      Save
+                    </Button>
+                    <Button
+                      size="sm"
+                      variant="bordered"
+                      color="danger"
+                      onPress={() => setIsEditing(!isEditing)}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                )}
+              </div>
             </CardHeader>
             <Divider />
-            <CardBody>Role: {user?.role}</CardBody>
+            <CardBody>
+              <div className="flex flex-col gap-4">
+                <Input
+                  label="Name"
+                  defaultValue={!isEditing ? user?.name : ""}
+                  placeholder="New name"
+                  isReadOnly={!isEditing}
+                  variant="underlined"
+                />
+                <Input
+                  label="Email"
+                  defaultValue={!isEditing ? user?.email : ""}
+                  placeholder="New email"
+                  isReadOnly={!isEditing}
+                  variant="underlined"
+                />
+                <Input
+                  label="Password"
+                  type="password"
+                  defaultValue={!isEditing ? "********" : ""}
+                  placeholder="New password"
+                  isReadOnly={!isEditing}
+                  variant="underlined"
+                />
+              </div>
+            </CardBody>
             <Divider />
             <CardFooter>
               Created at: {user?.created_at}
